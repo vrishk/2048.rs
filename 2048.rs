90859f306efdfa8c1a319c1904a5d67f4d599ee1
vrishk <krishnavrishab@gmail.com>
2020-10-17 23:08:52 +0530

    feat: random init, rotation

--- 2048.rs
+++ 2048.rs
@@ -1,13 +1,24 @@
 use std::fmt;
+use std::io;
+use std::time::SystemTime;
 
 // SECTION: Constants
 
 const N: usize = 4;
+const START_NUM: i32 = 3;
 
 // SECTION: Helper Functions
 
 fn get_color(n: u32) -> String {
+
     "\u{001b}[48;2;0;0;125m".to_string()
+}
+
+fn random() -> usize {
+    SystemTime::now()
+    .duration_since(SystemTime::UNIX_EPOCH)
+    .unwrap()
+    .as_micros() as usize
 }
 
 // SECTION: Main Struct
@@ -20,8 +31,27 @@ struct Grid {
 
 impl Grid {
     fn new(dim: usize) -> Self {
-        let grid = vec![vec![0; dim]; dim];
+        let mut grid = vec![vec![0; dim]; dim];
+        
+        for _ in 0..START_NUM {
+            let x = random() % 4;
+            let y = random() % 4;
+            grid[x][y] = 2;
+        }
+
         Grid {dim: dim, grid: grid}
+    }
+
+    fn rotate(&mut self) {
+        for i in 0..self.dim/2 {
+            for j in i..(self.dim-i-1) {
+                let temp = self.grid[i][j];
+                self.grid[i][j] = self.grid[j][self.dim-i-1];
+                self.grid[j][self.dim-i-1] = self.grid[self.dim-i-1][self.dim-j-1];
+                self.grid[self.dim-i-1][self.dim-j-1] = self.grid[self.dim-j-1][i];
+                self.grid[self.dim-j-1][i] = temp;
+            }
+        }
     }
 }
 
@@ -73,5 +103,11 @@ impl fmt::Display for Grid {
 
 fn main() {
     let mut grid = Grid::new(N);
-    println!("{}", grid)
+    println!("{}", grid);
+
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp).expect("ERROR: Improper input.");
+    
+    grid.rotate();
+    println!("{}", grid);
 }
