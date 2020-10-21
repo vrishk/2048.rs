96fc75e6b7d8673c497089306dae022c78fc1e55
vrishk <krishnavrishab@gmail.com>
2020-10-21 14:36:17 +0530

    chore: fmt

--- 2048.rs
+++ 2048.rs
@@ -11,18 +11,17 @@ const START_NUM: i32 = 1;
 
 // Hue Angle defined by value
 fn get_color(_n: u32) -> String {
-    
     "\u{001b}[48;2;0;0;125m".to_string()
 }
 
 // XORShift RNG
 fn random() -> usize {
     let mut r = SystemTime::now()
-    .duration_since(SystemTime::UNIX_EPOCH)
-    .unwrap()
-    .as_micros() as u32;
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_micros() as u32;
     r ^= r << 13;
-	r ^= r >> 17;
+    r ^= r >> 17;
     r ^= r << 5;
     r as usize
 }
@@ -38,12 +37,12 @@ struct Grid {
 impl Grid {
     fn new(dim: usize) -> Self {
         let mut grid = Grid {
-            dim: dim, 
-            grid: vec![vec![0; dim]; dim]
+            dim: dim,
+            grid: vec![vec![0; dim]; dim],
         };
 
         for _ in 0..START_NUM {
-            grid.add();   
+            grid.add();
         }
 
         grid
@@ -61,35 +60,37 @@ impl Grid {
     }
 
     fn rotate(&mut self) {
-        for i in 0..self.dim/2 {
-            for j in i..(self.dim-i-1) {
+        for i in 0..self.dim / 2 {
+            for j in i..(self.dim - i - 1) {
                 let temp = self.grid[i][j];
-                self.grid[i][j] = self.grid[j][self.dim-i-1];
-                self.grid[j][self.dim-i-1] = self.grid[self.dim-i-1][self.dim-j-1];
-                self.grid[self.dim-i-1][self.dim-j-1] = self.grid[self.dim-j-1][i];
-                self.grid[self.dim-j-1][i] = temp;
+                self.grid[i][j] = self.grid[j][self.dim - i - 1];
+                self.grid[j][self.dim - i - 1] = self.grid[self.dim - i - 1][self.dim - j - 1];
+                self.grid[self.dim - i - 1][self.dim - j - 1] = self.grid[self.dim - j - 1][i];
+                self.grid[self.dim - j - 1][i] = temp;
             }
         }
     }
 
     fn slide(&mut self) {
-        
         for i in 0..self.dim {
-            
             let mut j: usize = 0;
             while j < self.grid[i].len() {
                 if self.grid[i][j] == 0 {
                     self.grid[i].remove(j);
-                } else { j += 1; }
+                } else {
+                    j += 1;
+                }
             }
 
             let mut j: usize = 0;
 
-            while j+1 < self.grid[i].len() {
-                if self.grid[i][j] == self.grid[i][j+1] {
+            while j + 1 < self.grid[i].len() {
+                if self.grid[i][j] == self.grid[i][j + 1] {
                     self.grid[i][j] *= 2;
-                    self.grid[i].remove(j+1);
-                } else { j += 1; }
+                    self.grid[i].remove(j + 1);
+                } else {
+                    j += 1;
+                }
             }
             for _ in 0..(self.dim - self.grid[i].len()) {
                 self.grid[i].push(0);
@@ -104,7 +105,7 @@ impl Grid {
         self.rotate();
         self.rotate();
     }
-    
+
     fn down(&mut self) {
         self.rotate();
         self.rotate();
@@ -133,42 +134,37 @@ impl fmt::Display for Grid {
         for j in 0..self.grid.len() {
             for i in 0..self.grid[j].len() {
                 res = [
-                    res, 
-                    format!(
-                        "{}{}\u{001b}[0m", 
-                        get_color(self.grid[j][i]), 
-                        " ".repeat(7)
-                    )
-                ].join("");
+                    res,
+                    format!("{}{}\u{001b}[0m", get_color(self.grid[j][i]), " ".repeat(7)),
+                ]
+                .join("");
             }
             res += "\n";
             for i in 0..self.grid[j].len() {
-                let val = if self.grid[j][i] > 0 { 
-                    self.grid[j][i].to_string() 
+                let val = if self.grid[j][i] > 0 {
+                    self.grid[j][i].to_string()
                 } else {
                     ".".to_string()
                 };
                 res = [
                     res,
                     format!(
-                        "{}{}{}{}\u{001b}[0m", 
+                        "{}{}{}{}\u{001b}[0m",
                         get_color(self.grid[j][i]),
-                        " ".repeat(4-val.len()),
+                        " ".repeat(4 - val.len()),
                         val,
                         " ".repeat(3)
-                    )
-                ].join("");
+                    ),
+                ]
+                .join("");
             }
             res += "\n";
             for i in 0..self.grid[j].len() {
                 res = [
-                    res, 
-                    format!(
-                        "{}{}\u{001b}[0m", 
-                        get_color(self.grid[j][i]), 
-                        " ".repeat(7)
-                    )
-                ].join("");
+                    res,
+                    format!("{}{}\u{001b}[0m", get_color(self.grid[j][i]), " ".repeat(7)),
+                ]
+                .join("");
             }
             res += "\n";
         }
@@ -185,7 +181,9 @@ fn main() {
         grid.add();
         println!("{}", grid);
         let mut inp = String::new();
-        io::stdin().read_line(&mut inp).expect("ERROR: Improper input.");
+        io::stdin()
+            .read_line(&mut inp)
+            .expect("ERROR: Improper input.");
         match inp.as_str() {
             "w\n" => grid.up(),
             "s\n" => grid.down(),
